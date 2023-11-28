7e6cae1c3dc622346656540bac6fbac77bfc76d3
lindsaySchool <r0883883@student.thomasmore.be>
2023-11-28 18:07:37 +0100

    feat: add login component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,9 @@
 <script setup>
-
+import Login from './components/Login/Login.vue';
 </script>
 
 <template>
-  <header>
-    <img class="logo" src="./assets/logo.svg" alt="Sneaker Configurator" />
-    <div class="wrapper">
-      <h1>Sneaker Configurator</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-        voluptatum, quibusdam, quos, voluptatibus quia quae voluptates
-        voluptatem quod doloribus quas quibusdam, quos, voluptatibus quia quae
-        voluptates voluptatem quod doloribus quas.
-      </p>
-    </div>
-  </header>
+      <Login />
 </template>
 
 <style scoped>
