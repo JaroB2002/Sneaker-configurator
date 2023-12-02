5dfa5f4d9b57fa7b0d9fe0b542180c88f0b70d6c
JaroB2002 <116017813+JaroB2002@users.noreply.github.com>
2023-12-02 21:49:59 +0100

    Chore: change detailOrder layout

--- components/DetailOrder.vue
+++ components/DetailOrder.vue
@@ -1,24 +1,59 @@
 <template>
-    <div class="detail-order">
-      <div class="dashboard-header">
-        <h1>Detail Order</h1>
-      </div>
-      <div class="order-details">
-        <div class="detail-item">
-          <span class="label">Order ID:</span>
-          <span class="value">{{ order.id }}</span>
+    <div class="detail-order min-h-screen flex items-center justify-center bg-gray-100">
+      <div class="w-full max-w-md p-6 bg-white rounded-md shadow-md">
+        <div class="text-center">
+          <h1 class="text-2xl font-semibold text-gray-800">Order Details</h1>
         </div>
-        <div class="detail-item">
-          <span class="label">Product:</span>
-          <span class="value">{{ order.product }}</span>
-        </div>
-        <div class="detail-item">
-          <span class="label">Quantity:</span>
-          <span class="value">{{ order.quantity }}</span>
-        </div>
-        <div class="detail-item">
-          <span class="label">Total Price:</span>
-          <span class="value">{{ order.totalPrice }}</span>
+        <div class="mt-4">
+          <div class="flex justify-between mb-2">
+            <span class="text-gray-600">Order ID:</span>
+            <span class="font-medium">{{ order.id }}</span>
+          </div>
+          <div class="flex justify-between mb-2">
+            <span class="text-gray-600">Date:</span>
+            <span class="font-medium">{{ order.date }}</span>
+          </div>
+          <div class="flex justify-between mb-2">
+            <span class="text-gray-600">Product:</span>
+            <span class="font-medium">{{ order.product }}</span>
+          </div>
+          <div class="flex justify-between mb-2">
+            <span class="text-gray-600">Quantity:</span>
+            <span class="font-medium">{{ order.quantity }}</span>
+          </div>
+          <div class="flex justify-between mb-2">
+            <span class="text-gray-600">Total Price:</span>
+            <span class="font-medium">€{{ order.totalPrice.toFixed(2) }}</span>
+          </div>
+          <div class="flex justify-between mb-4">
+            <span class="text-gray-600">Status:</span>
+            <span
+              :class="{
+                'text-green-500': order.status === 'accepted',
+                'text-yellow-500': order.status === 'pending',
+                'text-red-500': order.status === 'denied',
+              }"
+            >
+              {{ order.status }}
+            </span>
+          </div>
+          <div class="mt-4">
+            <label
+              for="status"
+              class="block text-sm font-medium text-gray-700 mb-2"
+            >
+              Change Status:
+            </label>
+            <select
+              id="status"
+              v-model="order.status"
+              class="w-full p-2 border border-gray-300 rounded"
+            >
+              <option value="pending">Pending</option>
+              <option value="accepted">Accepted</option>
+              <option value="denied">Denied</option>
+            </select>
+          </div>
         </div>
       </div>
     </div>
@@ -29,42 +64,15 @@
   
   const order = ref({
     id: 1,
+    date: '2023-12-01',
     product: 'Example Product',
     quantity: 2,
     totalPrice: 50.0,
+    status: 'pending',
   });
   </script>
   
   <style scoped>
-  .detail-order {
-    max-width: 600px;
-    margin: 0 auto;
-  }
-  
-  .dashboard-header {
-    background-color: #3498db;
-    color: #fff;
-    padding: 20px;
-    text-align: center;
-  }
-  
-  .order-details {
-    padding: 20px;
-    border: 1px solid #ddd;
-    border-radius: 8px;
-    margin-top: 20px;
-  }
-  
-  .detail-item {
-    display: flex;
-    justify-content: space-between;
-    margin-bottom: 10px;
-  }
-  
-  .label {
-    font-weight: bold;
-  }
-  
   /* Voeg hier je scoped stijlen toe indien nodig */
   </style>
   
