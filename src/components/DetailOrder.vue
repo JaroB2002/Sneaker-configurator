54f58b2e11293fbd98e8d5684bd0a6d255bdeba7
JaroB2002 <116017813+JaroB2002@users.noreply.github.com>
2023-12-10 22:22:58 +0100

    Feat: Implement order deletion functionality and handle undefined totalPrice

--- src/components/DetailOrder.vue
+++ src/components/DetailOrder.vue
@@ -1,86 +1,96 @@
 <script setup>
-    import { ref } from 'vue'
-    import { useRouter } from 'vue-router'; // Add this import statement
+import { ref, computed } from 'vue';
+import { useRouter } from 'vue-router';
 
+const order = ref({
+  id: 1,
+  date: '2023-12-01',
+  product: 'Example Product',
+  quantity: 2,
+  totalPrice: 50.0,
+  status: 'pending',
+});
 
-    const order = ref({
-        id: 1,
-        date: '2023-12-01',
-        product: 'Example Product',
-        quantity: 2,
-        totalPrice: 50.0,
-        status: 'pending',
-    });
+const statusClass = computed(() => {
+  return {
+    'text-green-500': order.value.status === 'accepted',
+    'text-yellow-500': order.value.status === 'pending',
+    'text-red-500': order.value.status === 'denied',
+  };
+});
 
+const removeOrder = () => {
+  // Check if order.value is defined before accessing its properties
+  if (order.value) {
+    // Implement logic to remove the order from the system
+    // For demonstration purposes, we'll just log a message here
+    console.log('Order deleted:', order.value.id);
 
-
+    // Reset the order to an empty object
+    order.value = {};
+  }
+};
 </script>
 
 <template>
-    <div class="min-h-screen flex items-center justify-center bg-gray-100">
-     <div class="w-full max-w-md p-6 bg-white rounded-md shadow-md">
+  <div class="min-h-screen flex items-center justify-center bg-gray-100">
+    <div class="w-full max-w-md p-6 bg-white rounded-md shadow-md">
       <div class="text-center">
-        <h1 class="text-3xl font-bold text-green-500">Order Details</h1>
+        <h1 class="text-3xl font-bold text-green-500 mb-4">Order Details</h1>
       </div>
-      <div class="mt-4">
+      <div class="mb-4">
         <div class="flex justify-between mb-2">
           <span class="text-gray-700">Order ID:</span>
-          <span class="font-bold text-gray-800">{{ order.id }}</span>
+          <span class="font-bold text-gray-800">#56565656</span>
         </div>
         <div class="flex justify-between mb-2">
           <span class="text-gray-700">Date:</span>
-          <span class="font-bold text-gray-800">{{ order.date }}</span>
+          <span class="font-bold text-gray-800">10/12/2023</span>
         </div>
         <div class="flex justify-between mb-2">
           <span class="text-gray-700">Product:</span>
-          <span class="font-bold text-gray-800">{{ order.product }}</span>
+          <span class="font-bold text-gray-800">Sneakers S43</span>
         </div>
         <div class="flex justify-between mb-2">
           <span class="text-gray-700">Quantity:</span>
-          <span class="font-bold text-gray-800">{{ order.quantity }}</span>
+          <span class="font-bold text-gray-800">3</span>
         </div>
         <div class="flex justify-between mb-2">
           <span class="text-gray-700">Total Price:</span>
-          <span class="font-bold text-green-500">€{{ order.totalPrice.toFixed(2) }}</span>
+          <span v-if="order.totalPrice !== undefined" class="font-bold text-green-500">
+            €73
+          </span>
         </div>
         <div class="flex justify-between mb-4">
           <span class="text-gray-700">Status:</span>
-          <span
-            :class="{
-              'text-green-500': order.status === 'accepted',
-              'text-yellow-500': order.status === 'pending',
-              'text-red-500': order.status === 'denied',
-            }"
-            class="font-bold"
-          >
-            {{ order.status }}
-          </span>
+          <span :class="statusClass" class="font-bold">{{ order.status }}</span>
         </div>
-        <div class="mt-4">
-          <label
-            for="status"
-            class="block text-sm font-medium text-gray-700 mb-2"
-          >
+      </div>
+      <div class="mb-4">
+        <!-- Additional styling for a more modern look -->
+        <div class="flex items-center justify-between mb-4">
+          <label for="status" class="block text-sm font-medium text-gray-700" :id="statusLabelId">
             Change Status:
           </label>
           <select
             id="status"
-            v-model="order.status"
-            class="w-full p-2 border border-green-500 rounded text-green-500"
+            v-model.trim="order.status"
+            aria-labelledby="statusLabelId"
+            class="w-2/3 p-2 border border-green-500 rounded text-green-500 focus:outline-none focus:border-green-500 focus:ring focus:ring-green-200"
           >
-          <option value="pending" class="text-yellow-500">Pending</option>
+            <option value="pending" class="text-yellow-500">Pending</option>
             <option value="accepted">Accepted</option>
             <option value="denied" class="text-red-500">Denied</option>
           </select>
-          
         </div>
-      </div>          
-          <!-- Link to "/#/Orders/" with text "Go back" -->
-          <router-link to="/Orders">Go back</router-link>
-
+      </div>
+      <div class="text-center">
+        <button @click="removeOrder" class="w-full p-2 bg-red-500 text-white rounded hover:bg-red-600">
+          Remove Order
+        </button>
+        <!-- Link to "/#/Orders/" with text "Go back" -->
+        <router-link to="/Orders" class="text-blue-500">Go back</router-link>
+      </div>
     </div>
   </div>
 </template>
-
-<style scoped>
-</style>
