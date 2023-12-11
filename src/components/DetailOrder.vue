e59a8ef489a9ab1aaec25ed1470a9edc5454dada
JaroB2002 <116017813+JaroB2002@users.noreply.github.com>
2023-12-11 18:00:21 +0100

    Refactor: optimize Order Details component

--- src/components/DetailOrder.vue
+++ src/components/DetailOrder.vue
@@ -1,37 +1,3 @@
-<script setup>
-import { ref, computed } from 'vue';
-import { useRouter } from 'vue-router';
-
-const order = ref({
-  id: 1,
-  date: '2023-12-01',
-  product: 'Example Product',
-  quantity: 2,
-  totalPrice: 50.0,
-  status: 'pending',
-});
-
-const statusClass = computed(() => {
-  return {
-    'text-green-500': order.value.status === 'accepted',
-    'text-yellow-500': order.value.status === 'pending',
-    'text-red-500': order.value.status === 'denied',
-  };
-});
-
-const removeOrder = () => {
-  // Check if order.value is defined before accessing its properties
-  if (order.value) {
-    // Implement logic to remove the order from the system
-    // For demonstration purposes, we'll just log a message here
-    console.log('Order deleted:', order.value.id);
-
-    // Reset the order to an empty object
-    order.value = {};
-  }
-};
-</script>
-
 <template>
   <div class="min-h-screen flex items-center justify-center bg-gray-100">
     <div class="w-full max-w-md p-6 bg-white rounded-md shadow-md">
@@ -67,7 +33,6 @@ const removeOrder = () => {
         </div>
       </div>
       <div class="mb-4">
-        <!-- Additional styling for a more modern look -->
         <div class="flex items-center justify-between mb-4">
           <label for="status" class="block text-sm font-medium text-gray-700" :id="statusLabelId">
             Change Status:
@@ -76,7 +41,7 @@ const removeOrder = () => {
             id="status"
             v-model.trim="order.status"
             aria-labelledby="statusLabelId"
-            class="w-2/3 p-2 border border-green-500 rounded text-green-500 focus:outline-none focus:border-green-500 focus:ring focus:ring-green-200"
+            class="w-2/3 p-2 border rounded text-gray-800 focus:outline-none focus:border-green-500 focus:ring focus:ring-green-200"
           >
             <option value="pending" class="text-yellow-500">Pending</option>
             <option value="accepted">Accepted</option>
@@ -85,12 +50,56 @@ const removeOrder = () => {
         </div>
       </div>
       <div class="text-center">
-        <button @click="removeOrder" class="w-full p-2 bg-red-500 text-white rounded hover:bg-red-600">
+        <!-- Single button with confirmation dialog -->
+        <button @click="confirmRemoveOrder" class="w-full p-2 bg-red-500 text-white rounded hover:bg-red-600">
           Remove Order
         </button>
+
         <!-- Link to "/#/Orders/" with text "Go back" -->
         <router-link to="/Orders" class="text-blue-500">Go back</router-link>
       </div>
     </div>
   </div>
 </template>
+
+<script setup>
+import { ref, computed } from 'vue';
+import { useRouter } from 'vue-router';
+
+const order = ref({
+  id: 1,
+  date: '2023-12-01',
+  product: 'Example Product',
+  quantity: 2,
+  totalPrice: 50.0,
+  status: 'pending',
+});
+
+const statusClass = computed(() => {
+  return {
+    'text-green-500': order.value.status === 'accepted',
+    'text-yellow-500': order.value.status === 'pending',
+    'text-red-500': order.value.status === 'denied',
+  };
+});
+
+const removeOrder = () => {
+  // Check if order.value is defined before accessing its properties
+  if (order.value) {
+    // Implement logic to remove the order from the system
+    // For demonstration purposes, we'll just log a message here
+    console.log('Order deleted:', order.value.id);
+
+    // Reset the order to an empty object
+    order.value = {};
+  }
+};
+
+const confirmRemoveOrder = () => {
+  // Implement a confirmation dialog before removing the order
+  const confirmMessage = "Are you sure you want to remove this order?";
+  if (confirm(confirmMessage)) {
+    removeOrder();
+  }
+};
+</script>
