d53a9340973418af08b4e9ab64f8b68160fdbb55
JaroB2002 <116017813+JaroB2002@users.noreply.github.com>
2023-12-16 10:49:58 +0100

    Chore: change language input

--- src/components/ChangePassword.vue
+++ src/components/ChangePassword.vue
@@ -47,26 +47,26 @@ const updatePassword = async () => {
 
 <template>
   <div class="max-w-md mx-auto mt-8 p-6 bg-white shadow-md">
-    <h2 class="text-2xl font-semibold mb-6">Wachtwoord wijzigen</h2>
+    <h2 class="text-2xl font-semibold mb-6">Change Password</h2>
     <div v-if="successMessage" class="mb-4 text-green-500 font-bold">{{ successMessage }}</div>
     <form @submit.prevent="updatePassword">
       <div class="mb-4">
-        <label for="username" class="block text-gray-700 text-sm font-bold mb-2">Gebruikersnaam:</label>
+        <label for="username" class="block text-gray-700 text-sm font-bold mb-2">Username:</label>
         <input v-model="username" type="text" id="username" name="username" class="w-full px-4 py-2 border">
       </div>
       <div class="mb-4">
-        <label for="oldPassword" class="block text-gray-700 text-sm font-bold mb-2">Huidig wachtwoord:</label>
+        <label for="oldPassword" class="block text-gray-700 text-sm font-bold mb-2">Current Password:</label>
         <input v-model="oldPassword" type="password" id="oldPassword" name="oldPassword" class="w-full px-4 py-2 border">
       </div>
       <div class="mb-4">
-        <label for="newPassword" class="block text-gray-700 text-sm font-bold mb-2">Nieuw wachtwoord:</label>
+        <label for="newPassword" class="block text-gray-700 text-sm font-bold mb-2">New Password:</label>
         <input v-model="newPassword" type="password" id="newPassword" name="newPassword" class="w-full px-4 py-2 border">
       </div>
       <div class="mb-6">
-        <label for="confirmPassword" class="block text-gray-700 text-sm font-bold mb-2">Bevestig nieuw wachtwoord:</label>
+        <label for="confirmPassword" class="block text-gray-700 text-sm font-bold mb-2">Confirm New Password:</label>
         <input v-model="confirmPassword" type="password" id="confirmPassword" name="confirmPassword" class="w-full px-4 py-2 border">
       </div>
-      <button type="submit" class="bg-[#69FF47] text-white w-full max-w-md px-4 py-2 transition duration-300 ease-in-out hover:shadow-md">Wachtwoord wijzigen</button>
+      <button type="submit" class="bg-[#69FF47] text-white w-full max-w-md px-4 py-2 transition duration-300 ease-in-out hover:shadow-md">Change Password</button>
     </form>
   </div>
 </template>
