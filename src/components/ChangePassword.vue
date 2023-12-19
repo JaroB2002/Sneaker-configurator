a6f030c01864b765174f270445688ebdcb6c1806
JaroB2002 <116017813+JaroB2002@users.noreply.github.com>
2023-12-19 22:03:35 +0100

    Feat: Enhance password update security & display messages

--- src/components/ChangePassword.vue
+++ src/components/ChangePassword.vue
@@ -1,6 +1,8 @@
+
 <script setup>
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
+import zxcvbn from 'zxcvbn';
 
 const router = useRouter();
 const username = ref('');
@@ -11,13 +13,21 @@ const successMessage = ref('');
 const errorMessage = ref('');
 const logoutSuccessMessage = ref('');
 
-
 const updatePassword = async () => {
   try {
     if (newPassword.value !== confirmPassword.value) {
+      const passwordStrength = getPasswordStrength(newPassword.value);
+console.log('Password Strength:', passwordStrength);
       errorMessage.value = "New password does not match the confirmed password";
       return;
     }
+
+    const passwordStrength = getPasswordStrength(newPassword);
+    if (passwordStrength.score < 3) {
+      errorMessage.value = `Choose a stronger password: ${passwordStrength.feedback.suggestions.join(' ')}`;
+      return;
+    }
+
     const response = await fetch('https://sneaker-api-4zoy.onrender.com/admin/change-password', {
       method: 'PUT',
       headers: {
@@ -31,38 +41,69 @@ const updatePassword = async () => {
       })
     });
 
+    if (!response.ok) {
+      throw new Error(`Password update failed with status: ${response.status}`);
+    }
+
     const json = await response.json();
 
     if (json.status === 'success') {
-      successMessage.value = "Wachtwoord succesvol gewijzigd!";
-      //console.log('Success message:', successMessage.value);
-
-      // Add a delay before navigation (e.g., 2 seconds)
+      successMessage.value = "Password successfully changed!";
       setTimeout(() => {
         router.push('/');
       }, 2000);
     } else {
-      successMessage.value = '';
-      //console.log("Failed to update password");
+      errorMessage.value = `Failed to update password: ${json.message}`;
     }
   } catch (error) {
-    //console.error('Error during password update:', error);
+    console.error('Error during password update:', error);
+    errorMessage.value = 'An unexpected error occurred during the password update.';
   }
 };
 
 const logout = () => {
-
   logoutSuccessMessage.value = 'You have been successfully logged out. Redirecting...';
-
   setTimeout(() => {
     router.push('/');
-  }, 2000); // Redirect after 2 seconds (adjust the delay as needed)
+  }, 2000);
 };
+
 const goBack = () => {
   router.push('/orders');
 };
-</script>
 
+const getPasswordStrength = (password) => {
+  return zxcvbn(String(password));
+};
+
+const getStrengthWidth = (passwordStrength) => {
+  const percentage = (passwordStrength.score + 1) * 20; // Adjusted to a scale of 0-100
+  return `${percentage}%`;
+};
+
+const getStrengthColorClass = (score) => {
+  if (score < 2) return 'bg-red-500';
+  if (score < 3) return 'bg-orange-500';
+  return 'bg-green-500';
+};
+
+const getStrengthLabel = (score) => {
+  if (score < 2) return 'Very Weak';
+  if (score < 3) return 'Weak';
+  if (score < 4) return 'Fair';
+  if (score < 5) return 'Strong';
+  return 'Very Strong';
+};
+
+const getStrengthLabelClass = (score) => {
+  return score < 3 ? 'text-red-500' : (score < 4 ? 'text-orange-500' : 'text-green-500');
+};
+
+const hasSpecialCharacter = (password) => {
+  const specialCharacterRegex = /[!@#$%^&*(),.?":{}|<>]/;
+  return specialCharacterRegex.test(password);
+};
+</script>
 <template>
   <div class="max-w-md mx-auto mt-8 p-6 bg-white shadow-md">
     <!-- Change Password Section -->
@@ -84,13 +125,43 @@ const goBack = () => {
           <label for="newPassword" class="block text-gray-700 text-sm font-bold mb-2">New Password:</label>
           <input v-model="newPassword" type="password" id="newPassword" name="newPassword"
             class="w-full px-4 py-2 border">
+          <div v-if="newPassword" class="mt-2">
+            <p>Password Strength:</p>
+            <div class="flex items-center">
+              <div class="w-1/4 pr-4">
+                <div class="text-sm" :class="getStrengthLabelClass(getPasswordStrength(newPassword).score)">
+                  {{ getStrengthLabel(getPasswordStrength(newPassword).score) }}
+                </div>
+              </div>
+              <div class="w-3/4">
+                <div class="relative">
+                  <div :class="getStrengthColorClass(getPasswordStrength(newPassword).score)"
+                    :style="{ width: getStrengthWidth(getPasswordStrength(newPassword)) }"></div>
+                </div>
+              </div>
+            </div>
+            <div class="mt-2">
+              <p class="text-sm font-semibold">Suggestions:</p>
+              <ul v-if="getPasswordStrength(newPassword).feedback.suggestions.length">
+                <li v-for="suggestion in getPasswordStrength(newPassword).feedback.suggestions" :key="suggestion">
+                  {{ suggestion }}
+                </li>
+                <li v-if="!hasSpecialCharacter(newPassword)">Include at least one special character (e.g., @, #, $)</li>
+              </ul>
+              <p v-else>No specific suggestions.</p>
+            </div>
+          </div>
         </div>
         <div class="mb-6">
           <label for="confirmPassword" class="block text-gray-700 text-sm font-bold mb-2">Confirm New Password:</label>
           <input v-model="confirmPassword" type="password" id="confirmPassword" name="confirmPassword"
             class="w-full px-4 py-2 border">
+          <p v-if="confirmPassword && newPassword !== confirmPassword" class="text-red-500 mt-2">
+            Passwords do not match.
+          </p>
         </div>
         <button type="submit"
+          :disabled="getPasswordStrength(newPassword).score < 3"
           class="bg-[#69FF47] text-white w-full max-w-md px-4 py-2 transition duration-300 ease-in-out hover:shadow-md">Change
           Password</button>
       </form>
@@ -100,14 +171,13 @@ const goBack = () => {
       <h2 class="text-2xl font-semibold mb-2">Log Out</h2>
       <p>Ready to leave? Click the button below to log out.</p>
       <div class="flex justify-between mt-4">
-        <!-- Align the blue button to the left -->
         <button @click="goBack" class="px-4 py-2 bg-blue-500 text-white -md">No, go back</button>
-
         <button @click.prevent="logout" class="px-4 py-2 bg-red-500 text-white -md">Yes, log Out</button>
       </div>
       <div v-if="logoutSuccessMessage" class="mt-4 text-green-500 font-bold">{{ logoutSuccessMessage }}</div>
     </div>
   </div>
 </template>
+
 
 <style scoped></style>
